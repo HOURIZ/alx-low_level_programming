5d218011f0b0a5eda533aa9a3dc5df7cc9a0aa89
HOURIZ <musanayusuf95@gmail.com>
2022-07-04 13:44:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 /**
  * main - main block
  * Description: prints all single digit numbers of base 10
@@ -8,18 +10,18 @@
  */
 int main(void)
 {
-int i = 0;
-int j = 0;
-for (i = 0; i < 10; i++)
-{
-for (j = i + 1; j < 10; j++)
-{
-putchar(48 + i);
-putchar(48 + j);
-putchar(',');
-putchar(' ');
-}
-}
-putchar ('\n');
-return (0);
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
 }
